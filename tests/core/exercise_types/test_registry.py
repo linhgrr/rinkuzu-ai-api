f226338ed9d0426fba61100e1e82267777b56a3e
linhgrr <linh.nbt225732@sis.hust.edu.vn>
2026-06-15 01:12:40 +0700

    feat(exercise): add ExerciseTypeHandler ABC and registry

---
+++ tests/core/exercise_types/test_registry.py
@@ -0,0 +1,53 @@
+import pytest
+
+from api.core.learning.exercise_types.base import ExerciseTypeHandler
+from api.core.learning.exercise_types.models import ExerciseType
+from api.core.learning.exercise_types.registry import get_handler, register
+
+
+def test_register_and_get_handler_returns_fresh_instances():
+    @register
+    class _DummyHandler(ExerciseTypeHandler):
+        exercise_type = ExerciseType.MCQ
+        output_model = object
+        payload_model = object
+
+        def prompt_instruction(self):
+            return "i"
+
+        def negative_constraints(self):
+            return "n"
+
+        def explanation_guidance(self):
+            return "e"
+
+        def payload_from_output(self, result):
+            return result
+
+        def to_response_dict(self, exercise):
+            return {}
+
+        def evaluate(self, exercise, answer):
+            return (True, "")
+
+        def tutor_question(self, exercise):
+            return ""
+
+        def tutor_options(self, exercise):
+            return []
+
+        def serialize_answer(self, exercise, answer):
+            return None
+
+    a = get_handler(ExerciseType.MCQ)
+    b = get_handler(ExerciseType.MCQ)
+    assert isinstance(a, _DummyHandler)
+    assert a is not b  # fresh per call
+
+
+def test_get_handler_unknown_type_raises_keyerror():
+    class _Fake:
+        value = "does-not-exist"
+
+    with pytest.raises(KeyError):
+        get_handler(_Fake())
